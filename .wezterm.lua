a54788f3583addd09e0149c751e39e11f35ea1a2
linrongbin16 <linrongbin16@outlook.com>
2023-09-30 10:45:31 +0800

    feat: is_windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,19 @@
 function run_command(cmd, raw)
-    local f = assert(io.popen(cmd, 'r'))
-    local s = assert(f:read('*a'))
-    f:close()
-    if raw then return s end
-    s = string.gsub(s, '^%s+', '')
-    s = string.gsub(s, '%s+$', '')
-    s = string.gsub(s, '[\n\r]+', ' ')
-    return s
+	local f = assert(io.popen(cmd, "r"))
+	local s = assert(f:read("*a"))
+	f:close()
+	if raw then
+		return s
+	end
+	s = string.gsub(s, "^%s+", "")
+	s = string.gsub(s, "%s+$", "")
+	s = string.gsub(s, "[\n\r]+", " ")
+	return s
 end
 
-local os_name = run_command('uname')
-local is_linux = os_name:gmatch('^Linux') ~= nil
+local os_name = run_command("uname")
+local is_linux = os_name:gmatch("^Linux") ~= nil
+local is_windows = package.config:sub(1, 1) == "\\"
 
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
@@ -31,46 +34,45 @@ config.font = wezterm.font(CodeNewRomanFont)
 -- config.font = wezterm.font("Monaco Nerd Font Mono")
 config.font_size = 14.0
 if is_linux then
-    config.font_size = 11.0
+	config.font_size = 11.0
 end
 config.color_scheme = GruvboxDarkTheme
 config.use_fancy_tab_bar = true
 config.window_frame = {
-    font = wezterm.font(CodeNewRomanFont),
-    font_size = 13,
+	font = wezterm.font(CodeNewRomanFont),
+	font_size = 13,
 }
 
 local function get_process(tab)
-    local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
-    if string.find(process_name, "kubectl") then
-        process_name = "kubectl"
-    end
-    return wezterm.format({ { Text = string.format("[%s]", process_name) } })
+	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	if string.find(process_name, "kubectl") then
+		process_name = "kubectl"
+	end
+	return wezterm.format({ { Text = string.format("[%s]", process_name) } })
 end
 
 local function get_current_working_dir(tab)
-    local current_dir = tab.active_pane.current_working_dir
-    local home_dir = string.format("file://%s", os.getenv("HOME"))
-    if current_dir:find("^" .. home_dir) ~= nil then
-        return "~" .. current_dir:sub(#home_dir + 1, #current_dir)
-    else
-        -- return current_dir
-        return current_dir:sub(#"file:///", #current_dir)
-    end
+	local current_dir = tab.active_pane.current_working_dir
+	local home_dir = string.format("file://%s", os.getenv("HOME"))
+	if current_dir:find("^" .. home_dir) ~= nil then
+		return "~" .. current_dir:sub(#home_dir + 1, #current_dir)
+	else
+		-- return current_dir
+		return current_dir:sub(#"file:///", #current_dir)
+	end
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-    local title = string.format(" %s %s ", get_process(tab), get_current_working_dir(tab))
-    return {
-        { Text = title },
-    }
+	local title = string.format(" %s %s ", get_process(tab), get_current_working_dir(tab))
+	return {
+		{ Text = title },
+	}
 end)
 
 config.default_cwd = wezterm.home_dir
 
-local is_windows = package.config:sub(1, 1) == "\\"
 if is_windows then
-    config.default_prog = { "pwsh.exe" }
+	config.default_prog = { "pwsh.exe" }
 end
 
 -- and finally, return the configuration to wezterm
