7bb2578f6e5af188efbc8ee218be7a17a68ff784
linrongbin16 <linrongbin16@outlook.com>
2023-10-24 13:05:56 +0800

    feat(install): optimize nerdfont

--- deps/apt.sh
+++ deps/apt.sh
@@ -13,11 +13,11 @@ install_or_skip "sudo apt-get install -y automake" "automake"
 install_or_skip "sudo apt-get install -y pkg-config" "pkg-config"
 install_or_skip "sudo apt-get install -y cmake" "cmake"
 
-# install_or_skip "sudo apt-get install -y git" "git"
 # install latest git from ppa
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install git
+# sudo apt-get install git
+install_or_skip "sudo apt-get install -y git" "git"
 install_or_skip "sudo apt-get install -y curl" "curl"
 install_or_skip "sudo apt-get install -y wget" "wget"
 
@@ -35,4 +35,3 @@ install_or_skip "sudo apt-get install -y python3 python3-dev python3-venv python
 
 install_or_skip "sudo apt-get install -y vim" "vim"
 install_or_skip "sudo apt-get install -y zsh" "zsh"
-
