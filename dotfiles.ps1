581527594da66ac71b3b077ffeb51576cdcf3641
linrongbin16 <linrongbin16@outlook.com>
2024-05-09 01:09:50 +0800

    refactor: remove oh-my-posh theme

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -1,9 +1,5 @@
 # path
 $env:PATH="$env:PATH;$env:USERPROFILE\.dotfiles\bin"
 
-# oh-my-posh theme
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
-
 # alias
-Set-Alias -name gnvim -value neovide
 Set-Alias -name lg -value lazygit
