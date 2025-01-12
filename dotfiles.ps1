9a312b821aff24be68c5735b03eeb197ed8f5e43
linrongbin16 <linrongbin16@outlook.com>
2025-01-12 14:45:04 +0800

    fix(ps1): use powershell alias under windows platform

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -4,6 +4,12 @@ $env:PATH="$env:PATH;$env:USERPROFILE\.dotfiles\bin"
 # lazygit
 Set-Alias -name lg -value lazygit
 
+# eza
+function MyLs1 { eza -lh $args }
+function MyLs2 { eza -lha $args }
+Set-Alias -name l -value MyLs1
+Set-Alias -name ll -value MyLs2
+
 # zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
