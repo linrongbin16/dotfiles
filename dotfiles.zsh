2affa5281186a744371fbb00e3f617137896f502
linrongbin16 <linrongbin16@outlook.com>
2025-04-07 14:59:22 +0800

    Support warp terminal app

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -42,9 +42,6 @@ alias gb="git branch"
 alias gm="git merge"
 alias gc="git commit"
 
-# dotfiles
-export PATH="$PATH:$HOME/.dotfiles/bin"
-
 # golang
 if [ "$IS_MAC" != "1" ]; then
   export GOROOT="$HOME/.go" # where go is installed
@@ -60,29 +57,35 @@ export PATH="$PATH:$HOME/.local/share/bob/nvim-bin"
 eval "$(zoxide init --cmd cd zsh)"
 
 # atuin
-eval "$(atuin init zsh)"
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+  eval "$(atuin init zsh)"
+else
+  eval "$(atuin init --disable-ctrl-r --disable-up-arrow zsh)"
+fi
 
 # mise
 eval "$(~/.local/bin/mise activate zsh)"
 
-# pure
-fpath+=($HOME/.zsh/pure)
-autoload -U promptinit; promptinit
-zstyle :prompt:pure:prompt:success color green
-zstyle :prompt:pure:git:stash show yes
-prompt pure
+if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
+  # pure
+  fpath+=($HOME/.zsh/pure)
+  autoload -U promptinit; promptinit
+  zstyle :prompt:pure:prompt:success color green
+  zstyle :prompt:pure:git:stash show yes
+  prompt pure
 
-# fzf-tab
-autoload -U compinit; compinit
-source $HOME/.zsh/fzf-tab/fzf-tab.plugin.zsh
-# disable sort when completing `git checkout`
-zstyle ':completion:*:git-checkout:*' sort false
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
-zstyle ':completion:*' menu no
-# preview directory's content with eza when completing cd/z
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza -1 --color=always $realpath'
-# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
-zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+  # fzf-tab
+  autoload -U compinit; compinit
+  source $HOME/.zsh/fzf-tab/fzf-tab.plugin.zsh
+  # disable sort when completing `git checkout`
+  zstyle ':completion:*:git-checkout:*' sort false
+  # set list-colors to enable filename colorizing
+  zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+  # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+  zstyle ':completion:*' menu no
+  # preview directory's content with eza when completing cd/z
+  zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+  zstyle ':fzf-tab:complete:z:*' fzf-preview 'eza -1 --color=always $realpath'
+  # NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+  zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+fi
