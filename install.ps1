26ab109897e1b6a8c9065c80ebb45a2734a6910c
linrongbin16 <linrongbin16@outlook.com>
2025-07-25 15:55:33 +0800

    chore

--- install.ps1
+++ install.ps1
@@ -52,7 +52,6 @@ Install -command "scoop install gzip" -target "gzip"
 Install -command "scoop install unzip" -target "unzip"
 
 Install -command "scoop install extras/alacritty" -target "vim"
-Install -command "scoop install vim" -target "vim"
 Install -command "scoop install starship" -target "starship"
 Install -command "scoop install mise" -target "mise"
 Install -command "scoop install pipx" -target "pipx"
