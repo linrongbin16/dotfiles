6c7600c45ecce5fe9f82cd98a1865006ce884dd3
linrongbin16 <linrongbin16@outlook.com>
2025-07-26 08:24:05 +0800

    Update dotfiles.zsh

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -76,6 +76,7 @@ eval "$(~/.local/bin/mise activate zsh)"
 init_prompt() {
   AGKOZAK_PROMPT_DIRTRIM=0
   AGKOZAK_LEFT_PROMPT_ONLY=1
+  AGKOZAK_CUSTOM_SYMBOLS=( '⇣⇡' '⇣' '⇡' '+' 'x' '!' '>' '?' 'S')
   source ~/.zsh/agkozak-zsh-prompt/agkozak-zsh-prompt.plugin.zsh
 }
 
