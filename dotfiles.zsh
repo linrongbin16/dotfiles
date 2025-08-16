604c546da2d0c535bc0e7ee7f56571a8a165b643
linrongbin16 <linrongbin16@outlook.com>
2025-08-16 23:53:44 +0800

    Fix deno and prompt (#25)

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -34,10 +34,10 @@ export GOPATH="$HOME/go"  # user workspace
 export PATH="$PATH:$GOPATH/bin"
 
 # deno/bun
-if [ -x "$HOME/.deno/bin/deno" ]; then
-  export PATH="$PATH:$HOME/.deno/bin"
+if [ -f "$HOME/.deno/env" ]; then
+  . "$HOME/.deno/env"
 fi
-if [ -d "$HOME/.bun/bin/bun" ]; then
+if [ -x "$HOME/.bun/bin/bun" ]; then
   export PATH="$PATH:$HOME/.bun/bin"
 fi
 
