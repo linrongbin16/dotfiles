2cfab8ed8f45abff9af5a4711b5763645e0c869c
linrongbin16 <linrongbin16@outlook.com>
2025-12-15 09:44:04 +0800

    Install mold linker for linux

--- inst/pacman.sh
+++ inst/pacman.sh
@@ -8,7 +8,7 @@ info "arch: $ARCH, arm: $IS_ARM"
 sudo pacman -Syy
 
 yes | sudo pacman -S base-devel
-yes | sudo pacman -S llvm clang lld
+yes | sudo pacman -S llvm clang lld mold
 install "yes | sudo pacman -S autoconf" "autoconf"
 install "yes | sudo pacman -S automake" "automake"
 install "yes | sudo pacman -S pkg-config" "pkg-config"
